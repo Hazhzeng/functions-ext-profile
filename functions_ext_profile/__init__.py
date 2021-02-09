30c8981e6b1be324f2a4eeb9b0953ecb4aa9082e
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2021-02-09 12:38:27 -0800

    Add IO read write check

--- functions_ext_profile/__init__.py
+++ functions_ext_profile/__init__.py
@@ -1,23 +1,45 @@
-from typing import Dict
+import os
+import gc
+from psutil import Process
 from logging import Logger
 from azure.functions import FuncExtension, Context
-from psutil import virtual_memory
 
 
 class ProfileExtension(FuncExtension):
 
     def __init__(self, filename: str):
         super().__init__(filename)
-        self.memory_case: Dict[str, float] = {}
+        self.pid: int = os.getpid()
+        self.process: Process = Process(self.pid)
+        self.mem_start = None
+
+        self.io_read_start = None
+        self.io_write_start = None
+        self.io_read_bytes_start = None
+        self.io_write_bytes_start = None
+
+        gc.disable()
 
     def before_invocation(self, logger: Logger, context: Context, *args, **kwargs) -> None:
-        mem = virtual_memory()
-        self.memory_case[context.function_name] = mem.percent
-        return super().before_invocation(logger, context, *args, **kwargs)
+        gc.collect()
+        self.mem_start = self.process.memory_info().rss
+
+        counter = self.process.io_counters()
+        self.io_read_start = counter.read_count
+        self.io_write_start = counter.write_count
+        self.io_read_bytes_start = counter.read_bytes
+        self.io_write_bytes_start = counter.write_bytes
 
     def after_invocation(self, logger: Logger, context: Context, *args, **kwargs) -> None:
-        mem = virtual_memory()
-        memory_utiliazation = mem.percent - self.memory_case[context.function_name]
-        del self.memory_case[context.function_name]
-        logger.info(f'{context.function_name} used memory percentage {memory_utiliazation}%')
-        return super().after_invocation(logger, context, *args, **kwargs)
+        mem_end = self.process.memory_info().rss
+
+        counter = self.process.io_counters()
+        io_read_end = counter.read_count
+        io_write_end = counter.write_count
+        io_read_bytes_end = counter.read_bytes
+        io_write_bytes_end = counter.write_bytes
+
+        logger.info(f'{context.function_name} Stats: '
+                    f'[Memory Usage {mem_end - self.mem_start} bytes] '
+                    f'[IO Read/Write Count {io_read_end - self.io_read_start}/{io_write_end - self.io_write_start}] '
+                    f'[IO Read/Write Bytes {io_read_bytes_end - self.io_read_bytes_start}/{io_write_bytes_end - self.io_write_bytes_start}]')
