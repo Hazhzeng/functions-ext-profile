5052034350cb8e4926b37bc36730fe3210455d91
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2021-03-09 00:58:42 -0800

    Only check io

--- functions_ext_profile/__init__.py
+++ functions_ext_profile/__init__.py
@@ -2,28 +2,22 @@ import os
 import gc
 from psutil import Process
 from logging import Logger
-from azure.functions import FuncExtension, Context
+from azure.functions import FuncExtensionBase, Context
 
 
-class ProfileExtension(FuncExtension):
+class ProfileExtension(FuncExtensionBase):
 
     def __init__(self, filename: str):
         super().__init__(filename)
         self.pid: int = os.getpid()
         self.process: Process = Process(self.pid)
-        self.mem_start = None
 
         self.io_read_start = None
         self.io_write_start = None
         self.io_read_bytes_start = None
         self.io_write_bytes_start = None
 
-        gc.disable()
-
     def before_invocation(self, logger: Logger, context: Context, *args, **kwargs) -> None:
-        gc.collect()
-        self.mem_start = self.process.memory_info().rss
-
         counter = self.process.io_counters()
         self.io_read_start = counter.read_count
         self.io_write_start = counter.write_count
@@ -31,8 +25,6 @@ class ProfileExtension(FuncExtension):
         self.io_write_bytes_start = counter.write_bytes
 
     def after_invocation(self, logger: Logger, context: Context, *args, **kwargs) -> None:
-        mem_end = self.process.memory_info().rss
-
         counter = self.process.io_counters()
         io_read_end = counter.read_count
         io_write_end = counter.write_count
@@ -40,6 +32,5 @@ class ProfileExtension(FuncExtension):
         io_write_bytes_end = counter.write_bytes
 
         logger.info(f'{context.function_name} Stats: '
-                    f'[Memory Usage {mem_end - self.mem_start} bytes] '
                     f'[IO Read/Write Count {io_read_end - self.io_read_start}/{io_write_end - self.io_write_start}] '
                     f'[IO Read/Write Bytes {io_read_bytes_end - self.io_read_bytes_start}/{io_write_bytes_end - self.io_write_bytes_start}]')
